f07b568f5ae144b45f145851acf1d56a22c07cc0
Damhan91 <damhanp@gmail.com>
2022-01-29 16:24:00 +0000

    add data models and admin to timetable app

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Timetable(models.Model):
+    name = models.CharField('Class Name', max_length=100)
+    class_date = models.DateTimeField('Class Date')
+    equipment_needed = models.CharField('Equipment Needed', max_length=100)
+
