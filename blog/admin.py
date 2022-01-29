1dd2b6d53efd618d2eb340c8af855cc43d196c45
Damhan91 <damhanp@gmail.com>
2022-01-29 13:06:04 +0000

    add search bar to admin pannel

--- blog/admin.py
+++ blog/admin.py
@@ -7,4 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ('status', 'created_on')
     summernote_field = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
