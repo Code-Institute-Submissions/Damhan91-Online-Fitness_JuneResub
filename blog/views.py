aa924f7eef740a68368c7787c40382ffc34b6916
Damhan91 <damhanp@gmail.com>
2022-02-03 21:32:24 +0000

    connected style.css£

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+class Postlist(generic.ListView):
+    model = Post
+    template_name = 'index.html'
+    ordering = ['-created_on']
+    paginate_by = 4
--- timetable/models.py
+++ timetable/models.py
@@ -5,3 +5,6 @@ class Timetable(models.Model):
     name = models.CharField('Class Name', max_length=100)
     class_date = models.DateTimeField('Class Date')
     equipment_needed = models.CharField('Equipment Needed', max_length=100)
+
+    def __str__(self):
+        return self.name
