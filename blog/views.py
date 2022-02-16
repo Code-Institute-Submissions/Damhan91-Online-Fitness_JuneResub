9dc5467ca0d5597e5963442bad2bbc9c18c5b7a7
Damhan91 <damhanp@gmail.com>
2022-02-16 19:44:14 +0000

    add blog.html

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,10 @@ from .models import Post
 
 class Postlist(generic.ListView):
     model = Post
-    template_name = 'base.html'
+    template_name = 'blog.html'
     ordering = ['-created_on']
     paginate_by = 4
+
 
 def hompage(request):
     return render(request, 'home.html')
