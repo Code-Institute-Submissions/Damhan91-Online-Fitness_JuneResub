7407a0b7e074ce467ba1f31e0975d980850bfee4
Damhan91 <damhanp@gmail.com>
2022-02-23 20:04:32 +0000

    add allauth

--- onlinefitness/settings.py
+++ onlinefitness/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -47,6 +51,11 @@ INSTALLED_APPS = [
     'blog',
     'timetable',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT = '/'
+LOGOUT_REDIRECT = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
