911de64285cd72b021c8a84d963fae83cb74ee33
Damhan91 <damhanp@gmail.com>
2022-03-09 11:55:28 +0000

    add update and delete view to views.py

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 from .forms import CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class PostList(generic.ListView):
